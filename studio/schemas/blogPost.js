dc8d9cc30f24ed96df3f904b644114a2f03ea41e
Christopher Tse <chris@chris-tse.com>
2020-05-27 22:54:21 -0500

    Add sanity studio with schemas

---
+++ studio/schemas/blogPost.js
@@ -0,0 +1,36 @@
+export default {
+    name: 'blogPost',
+    title: 'Blog post',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'URL',
+            type: 'string',
+        },
+        {
+            name: 'date',
+            title: 'Date Posted',
+            type: 'date',
+            options: {
+                dateFormat: 'YYYY/MM/DD',
+            },
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'array',
+            of: [{ type: 'block' }, { type: 'image' }],
+        },
+    ],
+}
