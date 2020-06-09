cda8d7448c5a2d39bd93f2c88765eb000ccdbae0
Christopher Tse <chris@chris-tse.com>
2020-06-08 21:20:21 -0500

    Add shadows and border radius to images

--- src/components/project-item.js
+++ src/components/project-item.js
@@ -5,7 +5,12 @@ function Project({ name, description, thumbnailPath, demoUrl, sourceUrl }) {
     return (
         <div className="flex flex-col md:flex-row mb-8">
             <div className="p-8 pl-0">
-                <img src={thumbnailPath} style={{ maxWidth: 350 }} alt="Pomodoro timer project thumbnail" />
+                <img
+                    className="rounded shadow-xl"
+                    src={thumbnailPath}
+                    style={{ maxWidth: 350 }}
+                    alt="Pomodoro timer project thumbnail"
+                />
             </div>
             <div className="row-span-2 p-8 pl-0">
                 <h3 className="text-2xl mb-4">{name}</h3>
