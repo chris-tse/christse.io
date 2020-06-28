91809d2689c525371e92c4dec61dcc55b1b23c2b
Christopher Tse <chris@chris-tse.com>
2020-06-27 21:30:53 -0500

    Adjust mobile nav menu styles

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,25 @@ function Header() {
         }
     `)
 
+    const routeList = [
+        {
+            route: `/work`,
+            title: `Work`,
+        },
+        {
+            route: `/projects`,
+            title: `Projects`,
+        },
+        {
+            route: `/blog`,
+            title: `Blog`,
+        },
+        {
+            route: `/contact`,
+            title: `Contact`,
+        },
+    ]
+
     return (
         <header className="text-purple-900">
             <div className="flex flex-wrap items-center justify-between max-w-3xl p-4 mx-auto md:p-8">
@@ -23,7 +42,7 @@ function Header() {
                 </Link>
 
                 <button
-                    className="flex items-center block px-3 py-2 border border-white rounded md:hidden"
+                    className="flex items-center px-3 py-2 border border-white rounded md:hidden"
                     onClick={() => toggleExpansion(!isExpanded)}
                 >
                     <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -32,28 +51,17 @@ function Header() {
                     </svg>
                 </button>
 
-                <nav className={`${isExpanded ? `block` : `hidden`} md:block md:flex md:items-center w-full md:w-auto`}>
-                    {[
-                        {
-                            route: `/work`,
-                            title: `Work`,
-                        },
-                        {
-                            route: `/projects`,
-                            title: `Projects`,
-                        },
-                        {
-                            route: `/blog`,
-                            title: `Blog`,
-                        },
-                        {
-                            route: `/contact`,
-                            title: `Contact`,
-                        },
-                    ].map(link => (
+                <nav
+                    className={`${
+                        isExpanded ? `block` : `hidden`
+                    } md:flex md:items-center w-full md:w-auto md:shadow-none shadow-md p-4 md:mt-0 mt-2`}
+                >
+                    {routeList.map((link, index) => (
                         <Link
                             activeClassName="font-bold"
-                            className="block mt-4 no-underline md:inline-block md:mt-0 md:ml-6"
+                            className={`${
+                                index !== routeList.length - 1 ? 'mb-4 md:mb-0' : ''
+                            } block no-underline md:inline-block md:mt-0 md:ml-6`}
                             key={link.title}
                             to={link.route}
                             getProps={({ isPartiallyCurrent }) =>
