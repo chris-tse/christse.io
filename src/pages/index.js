a9ec2e36a0e1f86d4039951e01e2368dcbab9fe1
Christopher Tse <chris@chris-tse.com>
2020-06-27 22:10:09 -0500

    Fix homepage and add my face

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,40 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import catAndHumanIllustration from '../images/cat-and-human-illustration.svg'
+import face from '../images/face.jpg'
 import { Link } from 'gatsby'
 
 function IndexPage() {
     return (
         <Layout>
-            <SEO keywords={['html', 'css', 'javascript', 'es6', 'react', 'tailwindcss', 'gatsby']} title="Home" />
+            <SEO
+                keywords={[
+                    'html',
+                    'css',
+                    'javascript',
+                    'es6',
+                    'react',
+                    'tailwindcss',
+                    'gatsby',
+                    'Oklahoma City',
+                    'Oklahoma',
+                    'developer',
+                    'software developer',
+                    'software engineer',
+                    'engineer',
+                    'web developer',
+                    'websites',
+                ]}
+                title="Home"
+            />
 
             <section>
-                <img alt="bla" className="block w-1/2 mb-8" src={catAndHumanIllustration} />
+                <span className="inline-block w-1/4 border-gray-600 md:border-6 border-4 rounded-full overflow-hidden shadow-round">
+                    <img alt="Chris headshot" className="block" src={face} />
+                </span>
 
                 <h2 className="inline-block py-3 mb-4 text-2xl font-bold">
-                    Hi, I&apos;m Chris. I&apos;m a software engineer specializing in front end development.
+                    Hi, I&apos;m Chris! I&apos;m a software engineer specializing in front end development.
                 </h2>
 
                 <p className="leading-normal mb-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
     purge: ['./src/**/*.js'],
-    theme: {},
+    theme: {
+        extend: {
+            boxShadow: {
+                round: '5px 5px 5px rgba(0,0,0,0.1)',
+            },
+        },
+    },
     variants: {},
     // https://github.com/tailwindcss/custom-forms
     plugins: [require('@tailwindcss/custom-forms')],
