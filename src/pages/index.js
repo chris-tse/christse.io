dc1e1e59452b1730af4be2b1455bb3867c913fb3
Christopher Tse <chris@chris-tse.com>
2020-06-27 21:30:35 -0500

    Fix homepage verbiage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import catAndHumanIllustration from '../images/cat-and-human-illustration.svg'
+import { Link } from 'gatsby'
 
 function IndexPage() {
     return (
@@ -12,24 +13,30 @@ function IndexPage() {
             <section>
                 <img alt="bla" className="block w-1/2 mb-8" src={catAndHumanIllustration} />
 
-                <h2 className="inline-block py-3 mb-4 text-2xl font-bold">Hi there! Something more here. </h2>
+                <h2 className="inline-block py-3 mb-4 text-2xl font-bold">
+                    Hi, I&apos;m Chris. I&apos;m a software engineer specializing in front end development.
+                </h2>
 
                 <p className="leading-loose mb-4">
-                    Some placeholder text here pls fix{` `}
+                    I love doing creating things for the web. My main choice of tools currently are React, Sass/SCSS,
+                    Tailwind CSS, and Node.js. I was born and raised in Calgary, Canada and now reside in Oklahoma City
+                    working at{' '}
                     <a
-                        className="font-bold text-purple-800 no-underline"
-                        href="https://tailwindcss.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
+                        className="text-purple-800 font-bold hover:text-purple-600 hover:underline"
+                        href="https://netsuite.com"
                     >
-                        Tailwind CSS
-                    </a>{' '}
-                    future me.
-                    <br />
-                    lksdfjklsdjflksdf
+                        Oracle Netsuite
+                    </a>
+                    .
                 </p>
 
-                <p>Some other stuff.</p>
+                <p>
+                    If you&apos;d like to get in touch, feel free to{' '}
+                    <Link className="text-purple-800 font-bold hover:text-purple-600 hover:underline" to="/contact">
+                        leave me a message
+                    </Link>
+                    .
+                </p>
             </section>
         </Layout>
     )
